b094f308894e110710819ac7f5b36652d7a39b82
sumityadav1509 <sumityadav2005@gmail.com>
2020-12-18 20:01:10 +0000

    Corrected the error before comparison between str and int, now converted str to int using the int() function.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ o = lower_case_string.count("o")
 v = lower_case_string.count("v") 
 e = lower_case_string.count("e") 
 love=l+o+v+e 
-love_score= str(true) + str(love) 
+love_score= int(str(true) + str(love)) 
 
 if love_score<10 or love_score>90 :
   print(f"Your love score is {love_score} , you go together like coke and mentos") 
@@ -25,5 +25,5 @@ elif love_score>=40 and love_score<=50 :
   print(f"Your love score is {love_score} , you are alright together") 
 
 else: 
-  print(f"Your love socre is {love_score}")
+  print(f"Your love score is {love_score}")
  
